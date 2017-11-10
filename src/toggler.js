b9ad00c1571e71d99b1a36f83716b1d839ec23b9
Augustin Riedinger <augustin.riedinger@gmail.com>
2017-11-10 12:10:32 +0100

    fixed

--- src/toggler.js
+++ src/toggler.js
@@ -1,18 +1,19 @@
 (function(){
   var toggleLinksTargetBlank = function(isToggled) {
+    var dataAttr = 'data-initial-target-blank';
     var $links = document.getElementsByTagName('a');
     for (var i=0; i<$links.length; i++) {
       var $link = $links[i];
       if (isToggled) {
-        $link.setAttribute('data-external', $link.getAttribute('target') === '_blank');
+        $link.setAttribute(dataAttr, $link.getAttribute(dataAttr) || ($link.getAttribute('target') === '_blank'));
       }
-      if (($link.getAttribute('data-external') === 'true') === isToggled) {
+      if (($link.getAttribute(dataAttr) === 'true') === isToggled) {
         $link.removeAttribute('target');
       } else {
         $link.setAttribute('target', $link.getAttribute('target') || '_blank');
       }
       if (!isToggled) {
-        $link.removeAttribute('data-external');
+        $link.removeAttribute(dataAttr);
       }
     }
   };
@@ -21,18 +22,28 @@
     return e.which === 88;
   };
 
-  if (document.documentElement.classList.contains('link-toggler'))
+  if (document.documentElement.classList.contains('link-toggler')) {
+    console.log('Behavior already loaded on website')
     return;
+  }
 
   document.addEventListener('keydown', function(downEvent){
-    if (!downEvent.repeat && needsToggle(downEvent)) {
-      toggleLinksTargetBlank(true);
-      var onceListener = function(upEvent) {
-        toggleLinksTargetBlank(false);
-        document.removeEventListener('keyup', onceListener);
-      };
-      document.addEventListener('keyup', onceListener);
+    if (needsToggle(downEvent)) {
+      if (downEvent.repeat) {
+        // Necessary otherwise target="_blank" is not effective
+        downEvent.preventDefault();
+      } else {
+        toggleLinksTargetBlank(true);
+        var onceListener = function(upEvent) {
+          console.log('Reset state');
+          window.removeEventListener('blur', onceListener);
+          document.removeEventListener('keyup', onceListener);
+          toggleLinksTargetBlank(false);
+        };
+        // Deactivate either on keyup or on blur to go to new window
+        document.addEventListener('keyup', onceListener);
+        window.addEventListener('blur', onceListener);
+      }
     }
   });
-
 })();
