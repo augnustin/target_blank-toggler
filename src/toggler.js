723c6db370fa90dbc4bb9b12291c291bf979469c
Augustin Riedinger <augustin.riedinger@gmail.com>
2017-11-14 17:40:23 +0100

    new version

--- src/toggler.js
+++ src/toggler.js
@@ -1,11 +1,26 @@
 (function(){
+  var findLink = function($node) {
+    return (!$node || ($node.tagName === 'A')) ? $node : findLink($node.parentNode);
+  };
+
   var toggleLinksTargetBlank = function(isToggled) {
     var dataAttr = 'data-initial-target-blank';
     var $links = document.getElementsByTagName('a');
+    var clickOpenInCurrentTab = function(clickEvent) {
+      var $link = findLink(clickEvent.target);
+      if ($link) {
+        window.location.href = $link.getAttribute('href');
+        clickEvent.preventDefault();
+      }
+    };
+
     for (var i=0; i<$links.length; i++) {
       var $link = $links[i];
       if (isToggled) {
         $link.setAttribute(dataAttr, $link.getAttribute(dataAttr) || ($link.getAttribute('target') === '_blank'));
+        if ($link.getAttribute(dataAttr) === 'true') {
+          $link.addEventListener('click', clickOpenInCurrentTab);
+        }
       }
       if (($link.getAttribute(dataAttr) === 'true') === isToggled) {
         $link.removeAttribute('target');
@@ -13,13 +28,16 @@
         $link.setAttribute('target', $link.getAttribute('target') || '_blank');
       }
       if (!isToggled) {
+        if ($link.getAttribute(dataAttr) === 'true') {
+          $link.removeEventListener('click', clickOpenInCurrentTab);
+        }
         $link.removeAttribute(dataAttr);
       }
     }
   };
 
   var needsToggle = function(e) {
-    return e.which === 88;
+    return (e.metaKey || e.ctrlKey) && e.shiftKey && e.code.match(/^(Shift|Control)/);
   };
 
   if (document.documentElement.classList.contains('link-toggler')) {
@@ -30,20 +48,15 @@
 
   document.addEventListener('keydown', function(downEvent){
     if (needsToggle(downEvent)) {
-      if (downEvent.repeat) {
-        // Necessary otherwise target="_blank" is not effective
-        downEvent.preventDefault();
-      } else {
-        toggleLinksTargetBlank(true);
-        var onceListener = function(upEvent) {
-          window.removeEventListener('blur', onceListener);
-          document.removeEventListener('keyup', onceListener);
-          toggleLinksTargetBlank(false);
-        };
-        // Deactivate either on keyup or on blur to go to new window
-        document.addEventListener('keyup', onceListener);
-        window.addEventListener('blur', onceListener);
-      }
+      toggleLinksTargetBlank(true);
+      var onceListener = function(upEvent) {
+        window.removeEventListener('blur', onceListener);
+        document.removeEventListener('keyup', onceListener);
+        toggleLinksTargetBlank(false);
+      };
+      // Deactivate either on keyup or on blur to go to new window
+      document.addEventListener('keyup', onceListener);
+      window.addEventListener('blur', onceListener);
     }
   });
 })();
